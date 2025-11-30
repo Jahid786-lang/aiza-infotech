c6061cd350d7040b66f2462b2288efb685dc253d
Jahid khan <Jahid@kifwat.com>
2025-11-30 15:30:33 +0530

    first commit

---
+++ src/constants/config.ts
@@ -0,0 +1,94 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Jahid Khan— Software Developer",
+    fullName: "Jahid khan",
+    email: "jak420786@gmail.com",
+  },
+  hero: {
+    name: "Jahid khan",
+    p: ["I develop mobile apps", "User interfaces and web applications"],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `I'm a Full Stack Developer with 3+ years of experience in React, React Native, Node.js, Express.js,
+Redux, MongoDB, SQL, and API development. Experienced in building scalable web and
+mobile applications, integrating payment gateways, optimizing performance, and deploying apps
+on the Google Play Store. Strong in problem-solving, UI/UX optimization, and delivering
+production-ready systems.`,
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    feedbacks: {
+      p: "What others say",
+      h2: "Testimonials.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
